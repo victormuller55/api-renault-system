559835304d6f582035e01ad8675580559f23cef3
Victor Muller <victormuller050@gmail.com>
2024-05-11 22:46:13 -0300

    m

--- src/main/java/com/api/renault/models/UserModel.java
+++ src/main/java/com/api/renault/models/UserModel.java
@@ -5,6 +5,7 @@ import jakarta.persistence.*;
 @Entity
 @Table(name = "tbl_renault_user")
 public class UserModel {
+    
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @JsonProperty("id")
--- src/main/java/com/api/renault/repository/UserRepository.java
+++ src/main/java/com/api/renault/repository/UserRepository.java
@@ -4,7 +4,6 @@ import com.api.renault.models.UserModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<UserModel, Integer> {
-
     UserModel findByLoginUser(String loginUser);
     boolean existsByLoginUser(String loginUser);
 }
